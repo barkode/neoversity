3d811f8895648432c15750a6bd78f85bf28f9ac2
barkode <konstantinks@gmail.com>
2026-04-06 16:55:31 +0100

    Improve date validation error message and add unit tests for get_days_from_today function

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from datetime import datetime
+from datetime import datetime, date
 
 def get_days_from_today(date: str) -> int:
     try:
         user_date = datetime.strptime(date, "%Y-%m-%d").date()
     except ValueError as e:
-        print(f"Sorry, {e}")
+        print(f"Not correct date '{date}'. {str(e).capitalize()}. Please, use date format as 'YYYY-MM-DD'.")
         return None
     delta_time = user_date - datetime.today().date()
     return delta_time.days
 
-get_days_from_today("2021-10-09") # −157
-get_days_from_today("2026-04-06") # 0 (today)
-get_days_from_today("2028-12-09") # positive (future)
-get_days_from_today("2020-20-20") # Error message, returns None
-get_days_from_today("2020-02-29") # Valid leap year date
-get_days_from_today("2021-02-29") # Error message, returns None
+assert get_days_from_today("2021-10-09") == -1640 # -1640
+
+d = str(date.today())
+assert get_days_from_today(d) == 0 # 0 (today)
+
+assert get_days_from_today("2028-12-09") > 0 # positive (future)
+
+assert get_days_from_today("2020-20-20") is None # Error message, returns None
+
+assert get_days_from_today("2021-02-29") is None # Valid leap year date
+assert get_days_from_today("2020-02-29") < 0 # Valid leap year date
+
+assert get_days_from_today("2021-02-29") is None # Error message, returns None
