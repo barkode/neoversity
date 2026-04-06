a2db44afbb8977612044503c398a54636de43b38
barkode <konstantinks@gmail.com>
2026-04-06 19:46:07 +0100

    Add additional test cases for get_days_from_today function in test_main.py

--- test_main.py
+++ test_main.py
@@ -6,29 +6,35 @@ from main import get_days_from_today, get_numbers_ticket
 
 class GetDaysFromToday(unittest.TestCase):
 	def test_past_day(self):
+		"""Test with a past date to ensure it returns a negative number of days."""
 		result = get_days_from_today("2021-10-09")
 		self.assertIsInstance(result, int)
 		self.assertEqual(result, -1640)
 
 	def test_today(self):
+		"""Test with a today date to ensure it returns zero number of days."""
 		result = get_days_from_today(str(date.today()))
 		self.assertIsInstance(result, int)
 		self.assertEqual(result, 0)
 
 	def test_future_day(self):
+		"""Test with a future date to ensure it returns a positive number of days."""
 		result = get_days_from_today("2028-12-09")
 		self.assertIsInstance(result, int)
 		self.assertGreater(result, 0)
 
 	def test_invalid_month(self):
+		"""Test with an invalid month to ensure it returns correct message."""
 		self.assertIsNone(get_days_from_today("2020-20-20"))
 
 	def test_valid_leap_year(self):
+		"""Test with a valid leap year to ensure it returns correct message."""
 		result = get_days_from_today("2020-02-29")
 		self.assertIsInstance(result, int)
 		self.assertLess(result, 0)
 
 	def test_invalid_leap_year(self):
+		"""Test with an invalid leap year to ensure it returns correct message."""
 		self.assertIsNone(get_days_from_today("2021-02-29"))
 
 
