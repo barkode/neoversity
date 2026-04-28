3f0e296de5c9527849f08349366869892a349ccf
barkode <konstantinks@gmail.com>
2026-04-28 16:57:29 +0100

    Refactor directory_colorizer.py to add colorization functions and improve code organization

--- goit-pycore-hw-04/directory_colorizer.py
+++ goit-pycore-hw-04/directory_colorizer.py
@@ -1,8 +1,22 @@
-from colorama import Fore, Style, Back
 import os
+import pathlib
+import sys
+from colorama import Fore, Back, Style, init
+
+# init(autoreset=True)
+def colorize_directory(path):
+    """Function colorize directory"""
+    pass
+
+
+def clear_screen():
+    """Function that clear console"""
+    os.system("clear" if os.name == "posix" else "cls")
+
+
 
 print(Fore.RED + 'some red text')
-print(Back.GREEN + 'and with a green background')
-print(Style.DIM + 'and in dim text')
-print(Style.RESET_ALL)
+# print(Back.GREEN + 'and with a green background')
+# print(Style.DIM + 'and in dim text')
+# print(Style.RESET_ALL)
 print('back to normal now')
