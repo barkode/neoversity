b67aa1b55532f767fa6af0ae2eab237deeaf6aa2
barkode <konstantinks@gmail.com>
2026-04-28 01:15:42 +0100

    Refactor total_salary function for improved readability and error handling

--- goit-pycore-hw-04/main.py
+++ goit-pycore-hw-04/main.py
@@ -1,35 +1,24 @@
 def total_salary(path: str) -> tuple[int, int]:
     """Function calculate total salary of given path"""
-    try:
-        salaries = []
-        with open(path, encoding='utf-8') as f:
-            for line in f:
-                line = line.strip()
-                if not line:
-                    continue
-                try:
-                    _, salary = line.split(',')
-                    salaries.append(float(salary))
-                except (ValueError, IndexError):
-                    print(f'Error in line {line}')
-                    continue
+    result = []
+    salaries = read_file(path)
 
-        if not salaries:
-            return 0, 0
-
-        total = sum(salaries)
-        average = total / len(salaries)
-        return int(total), int(average)
-    except FileNotFoundError:
-        print(f'File {path} not found')
+    if not salaries:
         return 0, 0
+
+    for salary in salaries:
+        _, person_salary = salary
+        result.append(float(person_salary))
+
+    total = sum(result)
+    average = total / len(salaries)
+    return int(total), int(average)
 
 
 def get_cats_info(path) -> list[dict]:
     """Function get cats info of given path"""
     result = []
     cats = read_file(path)
-    print(cats)
 
     if not cats:
         return []
@@ -61,9 +50,10 @@ def read_file(path) -> list[str]:
         print(f'File {path} not found')
         return []
 
-# total, average = total_salary("salary.txt")
-# print(
-#     f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
-#
+
+total, average = total_salary("salary.txt")
+print(
+    f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
 cats_info = get_cats_info("cats.txt")
 print(cats_info)
