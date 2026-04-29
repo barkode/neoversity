47abeeaefa97e7eae7124004fccc4ccc74219bbd
barkode <konstantinks@gmail.com>
2026-04-29 23:56:07 +0100

    Add contact management functions to helper_bot.py

--- goit-pycore-hw-04/helper_bot.py
+++ goit-pycore-hw-04/helper_bot.py
@@ -1,8 +1,22 @@
 def main():
     pass
 
+
 def parse_input():
     pass
+
+
+def add_contact():
+    pass
+
+
+def change_contact():
+    pass
+
+
+def show_phone():
+    pass
+
 
 if __name__ == '__main__':
     main()
