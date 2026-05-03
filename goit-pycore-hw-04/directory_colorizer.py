df2edf848476fd8ff3d779c4a3d6dbd6a0e850d0
barkode <konstantinks@gmail.com>
2026-05-03 19:21:47 +0100

    Refactor directory_colorizer.py to improve argument handling and add directory tree printing functionality

--- goit-pycore-hw-04/directory_colorizer.py
+++ goit-pycore-hw-04/directory_colorizer.py
@@ -1,34 +1,58 @@
-import os
-import pathlib
 import sys
-from colorama import Fore, Back, Style, init
-
-COLORS = {
-    }
-
-init(autoreset=True)
-def colorize_directory(path):
-    """Function colorize directory"""
-    pass
+import os
+from pathlib import Path
+from colorama import Fore, Style, init
+from helpers import COLOUR_SCHEMA
 
 
 def clear_screen():
-    """Function that clear console"""
-    os.system("clear" if os.name == "posix" else "cls")
+    """function that clears screen"""
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def print_directory_tree(path, indent=""):
+    for item in path.iterdir():
+        if item.is_dir():
+            colour = COLOUR_SCHEMA.get("directory")
+            icon = COLOUR_SCHEMA.get("icon_directory")
+            print(indent + colour + icon + item.name)
+            print_directory_tree(item, indent + "    ")
+        else:
+            ext = item.suffix.lower()
+            colour = COLOUR_SCHEMA.get(ext, COLOUR_SCHEMA["default"])
+            icon_file = COLOUR_SCHEMA.get("icon_file")
+            print(indent + colour + icon_file + item.name)
+
+
+def system_check():
+    """function that checks whether system is available"""
+    return "posix" if os.name == "posix" else "nt"
+
+
 def main():
-    if sys.argv != 2:
-        print(f"Використання: {sys.argv[0]} <шлях>", file=sys.stderr)
-        print(f"Отримано аргументів: {len(sys.argv) - 1}", file=sys.stderr)
-        sys.exit(1)
+    init(autoreset=True)
 
-    path = sys.argv[1]
-
-print(Fore.RED + 'some red text')
-print(Back.GREEN + 'and with a green background')
-print(Style.DIM + 'and in dim text')
-print(Style.RESET_ALL)
-print('back to normal now')
-
-if __name__ == '__main__':
     clear_screen()
+
+    if len(sys.argv) != 2:
+        print(f"Помилка: потрібно передати шлях до директорії.")
+        print(f"Приклад: python3 {sys.argv[0]} <шлях до директорії>")
+        return
+
+    directory_path = Path(sys.argv[1])
+
+    if not directory_path.exists():
+        print("Помилка: такого шляху не існує.")
+        return
+
+    if not directory_path.is_dir():
+        print("Помилка: вказаний шлях не є директорією.")
+        return
+
+    print(Fore.YELLOW + f"Структура директорії: {directory_path}")
+    print_directory_tree(directory_path)
+    print(Style.RESET_ALL)
+
+
+if __name__ == "__main__":
     main()
