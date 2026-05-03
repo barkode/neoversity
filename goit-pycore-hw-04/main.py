9cbb63aead2bcb49f7745cf3c37bef89cc98ac5d
barkode <konstantinks@gmail.com>
2026-05-03 21:38:17 +0100

    Refactor main.py to improve menu handling and integrate clear_screen function

--- goit-pycore-hw-04/main.py
+++ goit-pycore-hw-04/main.py
@@ -1,22 +1,49 @@
 import sys
-import calculate_total_salary
-import cats_info
+from constants import EXIT_PHRASES
+from helpers import clear_screen
 
-EXIT_PHRASES = ['quit', 'exit', 'q', 'logoff']
+
+
+def print_menu() -> None:
+    print(
+        "Press [1] to run Calculation Total Salary function (default file: salaries.txt)")
+    print(
+        "Press [2] to run Get Cats Info function (default file: cats_info.txt)")
+    print(
+        "Press [3] to run Directory Colorizer script (default directory: current directory)")
+    print("Press [4] to run CLI Helper Bot")
 
 
 def main():
+    clear_screen()
     while True:
-        print()
-        user_input = input("Введіть команду (або 'quit' для виходу): ").strip().lower()
-        if user_input in EXIT_PHRASES:
-            print("Вихід з програми. До побачення!")
-            break
-        else:
-            print(f"Ви ввели: {user_input}")
-    sys.exit(0)
+        print_menu()
+        user_input = input(
+            "Enter command (or 'quit' for exit): ").strip().lower()
+
+        match user_input:
+            case cmd if cmd in EXIT_PHRASES:
+                print("Goodbye!")
+                clear_screen()
+                break
+            case '1':
+                clear_screen()
+                print("CASE 1")
+                break
+            case '2':
+                clear_screen()
+                print("CASE 2")
+            case '3':
+                clear_screen()
+                print("CASE 3")
+            case '4':
+                clear_screen()
+                print("CASE 4")
+            case _:
+                clear_screen()
+                print(f"You enter: {user_input}")
+    sys.exit()
 
 
 if __name__ == "__main__":
     main()
-    sys.exit(0)
