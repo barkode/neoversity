c3459c09b799618cd061b2c615f2f93606c84361
barkode <konstantinks@gmail.com>
2026-05-09 22:41:26 +0100

    Enhance caching_fibonacci to handle base cases and improve cache initialization

--- goit-pycore-hw-05/fibo.py
+++ goit-pycore-hw-05/fibo.py
@@ -2,12 +2,19 @@ from typing import Callable
 
 
 def caching_fibonacci() -> Callable:
-    cache = {0: 0, 1: 1}
+    """Fibonacci caching function"""
+    cache = {}
 
     def fibonacci(n: int) -> int:
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
         if n in cache:
             return cache[n]
+
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+
         return cache[n]
 
     return fibonacci
