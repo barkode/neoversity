4dfae6fff87cf172d734403cba02f4176d32c45d
barkode <konstantinks@gmail.com>
2026-05-09 23:31:20 +0100

    Add log_analizer.py for log parsing and analysis functionality

---
+++ goit-pycore-hw-05/log_analizer.py
@@ -0,0 +1,19 @@
+def parse_log_line(line: str) -> dict:
+    """Function parse log line and return dict with data"""
+    pass
+
+def load_logs(file_path: str) -> list:
+    """Function load logs from file"""
+    pass
+
+def filter_logs_by_level(logs: list, level: str) -> list:
+    """Function filter logs by level"""
+    pass
+
+def count_logs_by_level(logs: list) -> dict:
+    """Function count logs by level"""
+    pass
+
+def display_log_counts(counts: dict) -> None:
+    """Function display log counts"""
+    pass
