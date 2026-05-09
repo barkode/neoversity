bbcd44e65af7148b7d455f0388e86c6b6757019e
barkode <konstantinks@gmail.com>
2026-05-09 22:24:18 +0100

    Refactor caching_fibonacci to return a callable and remove timer decorator

--- goit-pycore-hw-05/main.py
+++ goit-pycore-hw-05/main.py
@@ -1,21 +1,7 @@
-import time
-import functools
+from typing import Callable
 
 
-def timer(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.perf_counter()
-        result = func(*args, **kwargs)
-        end = time.perf_counter()
-        print(f"[{func.__name__}] виконано за {(end - start) * 1000:.4f} мс")
-        return result
-
-    return wrapper
-
-
-@timer
-def caching_fibonacci(number: int) -> int:
+def caching_fibonacci() -> Callable:
     cache = {0: 0, 1: 1}
 
     def fibonacci(n: int) -> int:
@@ -24,13 +10,4 @@ def caching_fibonacci(number: int) -> int:
         cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         return cache[n]
 
-    return fibonacci(number)
-
-
-print("BEFORE CACHE :")
-for i in range(101):
-    print(f"FOR number {i} : {caching_fibonacci(i)}")
-
-print("AFTER CACHE :")
-for i in range(101):
-    print(f"FOR number {i} : {caching_fibonacci(i)}")
+    return fibonacci
