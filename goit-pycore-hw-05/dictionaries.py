6db6c88e6d75eb964219918f30c820c0e93b220e
barkode <konstantinks@gmail.com>
2026-05-10 19:18:22 +0100

    Add dictionaries.py for message and log level definitions

---
+++ goit-pycore-hw-05/dictionaries.py
@@ -0,0 +1,12 @@
+messages_dictionary = {}
+
+level_dictionary = {
+    "info": 'info',
+    "warning": 'warning',
+    "error": 'error',
+    "debug": 'debug',
+    }
+
+colors_dictionary = {}
+
+EXIT_PHRASES = ['exit', 'close', 'bye', 'quit', 'q']
