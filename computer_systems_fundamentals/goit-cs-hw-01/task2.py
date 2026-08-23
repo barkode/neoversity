2dcd1624756e17e023f5e830a54e4d992b188a68
barkode <konstantinks@gmail.com>
2026-08-24 00:37:35 +0100

    Refactor parser to enhance grammar definitions and improve structure for arithmetic expressions

--- computer_systems_fundamentals/goit-cs-hw-01/task2.py
+++ computer_systems_fundamentals/goit-cs-hw-01/task2.py
@@ -106,6 +106,12 @@ class Num(AST):
 
 
 class Parser:
+    """Граматика
+        expr   : term ((PLUS | MINUS) term)*
+        term   : factor ((MUL | DIV) factor)*
+        factor : INTEGER | LPAREN expr RPAREN
+    """
+
     def __init__(self, lexer):
         self.lexer = lexer
         self.current_token = self.lexer.get_next_token()
@@ -123,14 +129,40 @@ class Parser:
         else:
             self.error()
 
-    def term(self):
-        """Парсер для 'term' правил граматики. У нашому випадку - це цілі числа."""
+    def factor(self):
+        """
+        factor: INTEGER | LPAREN expr RPAREN
+        """
         token = self.current_token
-        self.eat(TokenType.INTEGER)
-        return Num(token)
+
+        if token.type == TokenType.INTEGER:
+            self.eat(TokenType.INTEGER)
+            return Num(token)
+
+        if token.type == TokenType.LPAREN:
+            self.eat(TokenType.LPAREN)
+            node = self.expr()
+            self.eat(TokenType.RPAREN)
+            return node
+        self.error()
+
+    def term(self):
+        """ term : factor ((MUL | DIV) factor)*"""
+        node = self.factor()
+
+        while self.current_token.type in (TokenType.MUL, TokenType.DIV):
+            token = self.current_token
+            if token.type == TokenType.MUL:
+                self.eat(TokenType.MUL)
+            elif token.type == TokenType.DIV:
+                self.eat(TokenType.DIV)
+
+            node = BinOp(left=node, op=token, right=self.factor())
+
+        return node
 
     def expr(self):
-        """Парсер для арифметичних виразів."""
+        """expr : term ((PLUS | MINUS) term)*"""
         node = self.term()
 
         while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
@@ -139,8 +171,6 @@ class Parser:
                 self.eat(TokenType.PLUS)
             elif token.type == TokenType.MINUS:
                 self.eat(TokenType.MINUS)
-            elif token.type == TokenType.DIV:
-                self.eat(TokenType.DIV)
 
             node = BinOp(left=node, op=token, right=self.term())
 
@@ -153,13 +183,15 @@ def print_ast(node, level=0):
         print(f"{indent}Num({node.value})")
     elif isinstance(node, BinOp):
         print(f"{indent}BinOp:")
-        print(f"{indent}  left: ")
+        print(f"{indent}  left:")
         print_ast(node.left, level + 2)
         print(f"{indent}  op: {node.op.type}")
-        print(f"{indent}  right: ")
+        print(f"{indent}  right:")
         print_ast(node.right, level + 2)
     else:
         print(f"{indent}Unknown node type: {type(node)}")
+
+
 
 
 def main():
