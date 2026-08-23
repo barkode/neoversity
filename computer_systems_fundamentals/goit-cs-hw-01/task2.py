dad07d99e42f8bf50e5835ab7f612a7ab2a55862
barkode <konstantinks@gmail.com>
2026-08-24 00:22:32 +0100

    Add parser implementation for arithmetic expressions with AST structure

--- computer_systems_fundamentals/goit-cs-hw-01/task2.py
+++ computer_systems_fundamentals/goit-cs-hw-01/task2.py
@@ -88,6 +88,80 @@ class Lexer:
         return Token(TokenType.EOF, None)
 
 
+class AST:
+    pass
+
+
+class BinOp(AST):
+    def __init__(self, left, op, right) -> None:
+        self.left = left
+        self.op = op
+        self.right = right
+
+
+class Num(AST):
+    def __init__(self, token):
+        self.token = token
+        self.value = token.value
+
+
+class Parser:
+    def __init__(self, lexer):
+        self.lexer = lexer
+        self.current_token = self.lexer.get_next_token()
+
+    def error(self):
+        raise SyntaxError("Помилка синтаксичного аналізу")
+
+    def eat(self, token_type):
+        """
+        Порівнюємо поточний токен з очікуваним токеном і, якщо вони збігаються,
+        'поглинаємо' його і переходимо до наступного токена.
+        """
+        if self.current_token.type == token_type:
+            self.current_token = self.lexer.get_next_token()
+        else:
+            self.error()
+
+    def term(self):
+        """Парсер для 'term' правил граматики. У нашому випадку - це цілі числа."""
+        token = self.current_token
+        self.eat(TokenType.INTEGER)
+        return Num(token)
+
+    def expr(self):
+        """Парсер для арифметичних виразів."""
+        node = self.term()
+
+        while self.current_token.type in (TokenType.PLUS, TokenType.MINUS):
+            token = self.current_token
+            if token.type == TokenType.PLUS:
+                self.eat(TokenType.PLUS)
+            elif token.type == TokenType.MINUS:
+                self.eat(TokenType.MINUS)
+            elif token.type == TokenType.DIV:
+                self.eat(TokenType.DIV)
+
+            node = BinOp(left=node, op=token, right=self.term())
+
+        return node
+
+
+def print_ast(node, level=0):
+    indent = "  " * level
+    if isinstance(node, Num):
+        print(f"{indent}Num({node.value})")
+    elif isinstance(node, BinOp):
+        print(f"{indent}BinOp:")
+        print(f"{indent}  left: ")
+        print_ast(node.left, level + 2)
+        print(f"{indent}  op: {node.op.type}")
+        print(f"{indent}  right: ")
+        print_ast(node.right, level + 2)
+    else:
+        print(f"{indent}Unknown node type: {type(node)}")
+
+
 def main():
     while True:
         try:
@@ -96,10 +170,9 @@ def main():
                 print("Вихід із програми.")
                 break
             lexer = Lexer(text)
-            token = lexer.get_next_token()
-            while token.type != TokenType.EOF:
-                print(token)
-                token = lexer.get_next_token()
+            parser = Parser(lexer)
+            tree = parser.expr()
+            print_ast(tree)
         except Exception as e:
             print(e)
 
