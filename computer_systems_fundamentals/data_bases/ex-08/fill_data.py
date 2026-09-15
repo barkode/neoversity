7544e9d08bb58df40f65372b0bc3f74496e2e043
barkode <konstantinks@gmail.com>
2026-09-15 12:51:44 +0100

    Add data insertion functionality to SQLite database in fill_data.py

--- computer_systems_fundamentals/data_bases/ex-08/fill_data.py
+++ computer_systems_fundamentals/data_bases/ex-08/fill_data.py
@@ -1,3 +1,4 @@
+import sqlite3
 from datetime import datetime
 from random import choice, randint
 
@@ -10,38 +11,46 @@ NUMBER_POST = 5
 
 def generate_fake_data(number_companies, number_employees, number_post) -> \
         tuple[list, list, list]:
-    fake_companies = []
-    fake_employees = []
-    fake_posts = []
     fake_data = faker.Faker()
-
-    for _ in range(number_companies):
-        fake_companies.append(fake_data.company())
-
-    for _ in range(number_employees):
-        fake_employees.append(fake_data.name())
-
-    for _ in range(number_post):
-        fake_posts.append(fake_data.job())
+    fake_companies = [fake_data.company() for _ in range(number_companies)]
+    fake_employees = [fake_data.name() for _ in range(number_employees)]
+    fake_posts = [fake_data.job() for _ in range(number_post)]
 
     return fake_companies, fake_employees, fake_posts
 
 
 def prepare_data(companies, employees, posts) -> tuple[list, list, list]:
-    for_companies = []
-    for_employees = []
+    for_companies = [(c,) for c in companies]
+    for_employees = [(emp, choice(posts), randint(1, len(companies))) for emp
+                     in employees]
     for_payments = []
-
-    for company in companies:
-        for_companies.append((company,))
-
-    for employee in employees:
-        for_employees.append(
-            (employee, choice(posts), randint(1, NUMBER_COMPANIES)))
 
     for month in range(1, 12 + 1):
         payment_date = datetime(2021, month, randint(10, 20)).date()
-        for emp in range(1, NUMBER_EMPLOYEES + 1):
-            for_payments.append((emp, payment_date, randint(1000, 10000)))
+        print("PAYMENT DATE: ", payment_date)
+        for emp_id in range(1, len(employees) + 1):
+            for_payments.append((emp_id, payment_date, randint(1000, 10000)))
 
     return for_companies, for_employees, for_payments
+
+
+def insert_data_to_db(companies, employees, payments) -> None:
+    with sqlite3.connect("salary.db.db") as con:
+        cur = con.cursor()
+
+        sql_to_companies = """INSERT INTO companies(compani_name)
+                              VALUES ?"""
+        cur.executemany(sql_to_companies, companies)
+        sql_to_employees = """INSERT INTO employees(employee_name, post, company_id)
+                              VALUES (?, ?, ?)"""
+        cur.executemany(sql_to_employees, employees)
+        sql_to_payments = """INSERT INTO payments(employee_id, date_of, total)
+                             VALUES (?, ?, ?)"""
+        cur.executemany(sql_to_payments, payments)
+        con.commit()
+
+
+if __name__ == "__main__":
+    companies, employees, posts = prepare_data(
+        *generate_fake_data(NUMBER_COMPANIES, NUMBER_EMPLOYEES, NUMBER_POST))
+    insert_data_to_db(companies, employees, posts)
